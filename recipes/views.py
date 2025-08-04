698e012b3600e949dc8dab28e4024b7dbc8752d8
andarilhoii <andarilhoii@gmail.com>
2025-08-04 16:14:04 -0300

    removido_contato_sobre

---
+++ recipes/views.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import path
+
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
